d866b9681fd28a367d3ac2fa878a44e25da99a99
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-03-22 16:10:49 +0100

    added my roomtype controllers

---
+++ controllers/roomtype.controller.js
@@ -0,0 +1,28 @@
+const RoomtypeService = require('../services/roomtype.service');
+/* 
+This is the section of my application that handles client request received from routes, processes them and sends out a response to the client
+*/
+class RoomtypeController{
+    // create room
+    async createRoomtype (req, res) {
+
+    }
+
+    async updateRoomtype (req, res) {
+
+    }
+
+    async deleteRoomtype (req, res) {
+
+    }
+
+    async fetchOne (req, res) {
+        
+    }
+
+    async fetchMany (req, res) {
+        
+    }
+}
+
+module.exports = RoomtypeController;
