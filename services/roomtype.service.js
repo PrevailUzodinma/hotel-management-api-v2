5c82a10530c134a567854a109282aeb4a58bc78c
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-03-22 12:45:20 +0100

    setting up my roomtype services

---
+++ services/roomtype.service.js
@@ -0,0 +1,19 @@
+const Roomtype = require("../models/roomtype.model");
+
+/* This part of my code base handles reusable business logic of my application that can be used across different parts of my application, eg database queries.
+like CRUD operations*/
+
+// making these function calls asynchronous because database queries take some time
+
+class RoomtypeService {
+    // create a roomtype
+  async create() {}
+    // edit a roomtype
+  async update() {}
+    // delete a roomtype
+  async delete() {}
+    // get a single roomtype
+  async fetchOne() {}
+    // get all roomtypes
+  async fetchAll() {}
+}
