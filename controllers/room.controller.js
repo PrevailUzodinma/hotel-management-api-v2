548550b91c5330cf31cb6bd60c1f80107b5587ce
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-03-22 16:09:16 +0100

    added my room controllers

---
+++ controllers/room.controller.js
@@ -0,0 +1,28 @@
+const RoomService = require('../services/room.service');
+/* 
+This is the section of my application that handles client request received from routes, processes them and sends out a response to the client
+*/
+class RoomController{
+    // create room
+    async createRoom (req, res) {
+
+    }
+
+    async updateRoom (req, res) {
+
+    }
+
+    async deleteRoom (req, res) {
+
+    }
+
+    async fetchOne (req, res) {
+        
+    }
+
+    async fetchMany (req, res) {
+        
+    }
+}
+
+module.exports = RoomController;
