0d29911e6dc9fa6fe6fe89d122e83f16b7203e49
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-03-22 22:49:41 +0100

    modified app.js

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 3838;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use('/api/v1/', router);
+
 app.listen(port, () =>{
     console.log(`Server has started running on port ${port}`);
 })
