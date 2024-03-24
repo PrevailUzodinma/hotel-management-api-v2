3e67fc0c558fd988d852cf53f1ac7110e158b99a
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-03-24 23:54:30 +0100

    adding more functionality to user service

--- services/user.service.js
+++ services/user.service.js
@@ -1,16 +1,41 @@
-const bcrypt = require('bcrypt');
-const User = require('../models/user.model');
+const bcrypt = require("bcrypt");
+const User = require("../models/user.model");
 
-const registerUser = async (userData) =>{
+class UserService {
+    // create/register a user
+  async registerUser(userData) {
     const { username, password, role } = userData;
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User.create({
-        username,
-        password: hashedPassword,
-        role
-    })
+      username,
+      password: hashedPassword,
+      role,
+    });
 
     return newUser;
+  }
+  // edit a room
+  async update(id, userUpdate) {
+    return await User.findByIdAndUpdate(id, userUpdate, {
+      new: true,
+    });
+  }
+  // delete a room
+  async delete(id) {
+    return await User.findByIdAndDelete(id);
+  }
+  // fetch a single room by id
+  async fetchOneById(id) {
+    return await User.findById(id);
+  }
+  // fetch a single room by any filter
+  async fetchOne(query) {
+    return await User.findOne(query);
+  }
+  // fetch all rooms
+  async fetch(filter) {
+    return await User.find(filter);
+  }
 }
 
-module.exports = {registerUser};
+module.exports = new UserService();
