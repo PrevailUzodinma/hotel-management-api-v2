41c0087af389a9665d6f888024885e0eb423f208
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-03-27 11:14:02 +0100

    mending my user functionalities

--- services/user.service.js
+++ services/user.service.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 
 class UserService {
-    // create/register a user
+  // create/register a user
   async registerUser(userData) {
     const { username, password, role } = userData;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -14,25 +14,46 @@ class UserService {
 
     return newUser;
   }
-  // edit a room
+
+  // login a user
+
+  async loginUser(userData) {
+    const { username, enteredPassword } = userData;
+    const existingUser = User.findOne(username);
+    if (!existingUser) {
+      throw new Error("User does not exist");
+    }
+    // take user password and compare to entered password
+    const isPasswordValid = await bcrypt.compare(
+      enteredPassword,
+      existingUser.password
+    );
+    if (!isPasswordValid) {
+      throw new Error("Invalid Password");
+    }
+
+    return existingUser;
+  }
+  
+  // edit a user
   async update(id, userUpdate) {
     return await User.findByIdAndUpdate(id, userUpdate, {
       new: true,
     });
   }
-  // delete a room
+  // delete a user
   async delete(id) {
     return await User.findByIdAndDelete(id);
   }
-  // fetch a single room by id
+  // fetch a single user by id
   async fetchOneById(id) {
     return await User.findById(id);
   }
-  // fetch a single room by any filter
+  // fetch a single user by any filter
   async fetchOne(query) {
     return await User.findOne(query);
   }
-  // fetch all rooms
+  // fetch all users
   async fetch(filter) {
     return await User.find(filter);
   }
